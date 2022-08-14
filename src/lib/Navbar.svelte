c7a575763b21d6d659c6cfce6e3cb7f8b0919db6
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-08-14 21:02:55 +1200

    Fix errors

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,15 +1,18 @@
+<script>
+  import { base } from "$app/paths";
+</script>
 <nav class="top-0 z-50">
   <div
     class="text-gray-900 mx-auto relative flex justify-between align-middle w-full bg-sky-500 min-h-50 px-4 py-2"
   >
-    <a href="/" class="flex justify-start w-fit"
+    <a href="{base}/" class="flex justify-start w-fit"
       ><h1 class="text-2xl font-bold">Coding Challenge</h1></a
     >
     <ul class="flex float-right items-center space-x-2">
-      <a href="/scoreboard"
+      <a href="{base}/scoreboard"
         ><li class="ml-2 text-base hover:font-semibold transition-all">Scoreboard</li></a
       >
-      <a href="/run"
+      <a href="{base}/run"
         ><li class="ml-2 text-base hover:font-semibold transition-all">Run JavaScript D8</li></a
       >
     </ul>
