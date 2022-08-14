e79cdb2c6173de08c43c5f54490c0f9619f34861
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-08-14 16:26:12 +1200

    add base path

--- svelte.config.js
+++ svelte.config.js
@@ -11,6 +11,9 @@ const config = {
     prerender: {
       default: true
     },
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/coding-challenge" : ""
+    },
     adapter: adapter()
   }
 };
