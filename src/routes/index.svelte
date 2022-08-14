7b7905b8282c86add0ded26965ee651413d65ca5
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-08-14 16:04:54 +1200

    Add tailwindcss

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="font-bold text-4xl mb-2">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev" class="underline text-blue-500 hover:text-blue-500/75 transition">kit.svelte.dev</a> to read the documentation</p>
