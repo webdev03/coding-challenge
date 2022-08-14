fdc044900af1e199d95d8d6dd2ee462927388181
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-08-14 16:06:24 +1200

    Format

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,7 @@
 <script>
   import "$lib/tailwind.css";
 </script>
+
 <div class="p-2">
   <slot />
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
 <h1 class="font-bold text-4xl mb-2">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev" class="underline text-blue-500 hover:text-blue-500/75 transition">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a
+    href="https://kit.svelte.dev"
+    class="underline text-blue-500 hover:text-blue-500/75 transition">kit.svelte.dev</a
+  > to read the documentation
+</p>
